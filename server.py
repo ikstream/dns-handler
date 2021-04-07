a8ab3197063fcce4537b5ea3910e8b66a731ea5f
Stefan Venz <stefan.venz@protonmail.com>
2021-04-07 20:40:42 +0200

    Add License
    Signed-off-by: Stefan Venz <stefan.venz@protonmail.com>

--- server.py
+++ server.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+'''
+    Dalec collects data on user devices and transmitts them anonymously
+    Copyright (C) 2021  Stefan Venz <stefan.venz@protonmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+
+
 """
 This python script listens on UDP port 53
 for incoming DNS request and parses the questions
